14930f636df70d3dc9b7c51a2d3b153b99550c71
jiantang520star <jiantang726@gmail.com>
2026-07-17 20:57:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-const year = document.querySelector("#year");
+﻿const year = document.querySelector("#year");
 const topButton = document.querySelector(".top-button");
 
-year.textContent = new Date().getFullYear();
-
-topButton.addEventListener("click", () => {
-  window.scrollTo({ top: 0, behavior: "smooth" });
-});
+if (year) year.textContent = new Date().getFullYear();
+if (topButton) {
+  topButton.addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
+}
